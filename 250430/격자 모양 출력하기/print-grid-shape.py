ffdac43c0b6e9eb9b70af6fb32ea55d0bae0a861
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-04-30 12:42:21 +0900

    Title: 격자 모양 출력하기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/격자 모양 출력하기/print-grid-shape.py
+++ 250430/격자 모양 출력하기/print-grid-shape.py
@@ -5,12 +5,13 @@
 n, m = map(int, input().split())
 arr = [[0 for i in range(n)] for i in range(n)]
 
-#m개의 점들이 n번 존재 
-m_arr = [list(map(int, input().split())) for i in range(n)]
+#m개의 점들이 존재 
+m_arr = [list(map(int, input().split())) for i in range(m)]
 
 
-for i in range(n):
+for i in range(m): #모든 점에 대해 arr로 넣어줘야함
     #점은 각 두 값으로 계속 존재(행, 열)
+
     arr[m_arr[i][0] - 1][m_arr[i][1] - 1] = m_arr[i][0] * m_arr[i][1]
     
 for i in range(n):  
