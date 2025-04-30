5159c0d941ed6b27cf7b21fd9f41f26683743d19
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-04-30 13:35:54 +0900

    Title: 배열의 평균, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250430/배열의 평균/ave-of-array.py
@@ -0,0 +1,24 @@
+#2행 4열
+arr = [list(map(int, input().split())) for i in range(2)]
+sum = 0.0
+all_sum = 0.0
+
+for i in range(2):
+    sum = 0.0
+    for j in range(4): 
+        sum += arr[i][j] #각 행별로 
+        all_sum += arr[i][j]
+    print(sum / 4, end = " ") #각 4요소에 대한 평균
+print()
+
+for j in range(4):
+    sum = 0.0
+    for i in range(2): #각 열별로 
+        sum += arr[i][j] 
+    print(sum / 2, end = " ")
+print()
+
+print(all_sum / 8) #전체 8 요소 
+    
+
+
