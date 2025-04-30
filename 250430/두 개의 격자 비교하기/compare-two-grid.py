ddc8d5e5a635e9ea0b64d9b7c0d4d9249be933b2
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-04-30 14:10:10 +0900

    Title: 두 개의 격자 비교하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/두 개의 격자 비교하기/compare-two-grid.py
+++ 250430/두 개의 격자 비교하기/compare-two-grid.py
@@ -5,12 +5,12 @@ n, m = map(int, input().split())
 #m개의 원소를 2n번 
 m_arr = [list(map(int, input().split())) for i in range(2*n)]
 
-arr = [[0 for i in range(n)] for j in range(n)]
+arr = [[0 for i in range(m)] for j in range(n)]
 
 for i in range(n): 
-    for j in range(n):
+    for j in range(m):
 
-        if m_arr[i][j] != m_arr[i + 4][j]:
+        if m_arr[i][j] != m_arr[i + n][j]:
             arr[i][j] = 1
         print(arr[i][j], end = " ")
     print()
