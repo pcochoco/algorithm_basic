ad47f522eac1f180ab9553648a97dc60d2246e29
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-05-02 20:00:50 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250502/Date to Date/date-to-date.py
@@ -0,0 +1,18 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30 ]
+cnt = 0 
+while True:
+    if m1 == m2 and d1 == d2: 
+        break
+    cnt += 1
+    d1 += 1  
+
+    if d1 > num_of_days[m1]: 
+        #증가했을 때 day 가 크면 
+        #실제로는 month 넘어간 날짜인 것 
+
+        m1 += 1  
+        d1 = 1 
+    
+print(cnt)
