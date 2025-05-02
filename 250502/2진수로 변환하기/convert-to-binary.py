42e5f27d0b7099a837297a288967106320b8daff
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-05-02 20:25:34 +0900

    Title: 2진수로 변환하기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250502/2진수로 변환하기/convert-to-binary.py
+++ 250502/2진수로 변환하기/convert-to-binary.py
@@ -1,8 +1,12 @@
 n = int(input())
 arr = []
+
+if n == 0:
+    print(0)
+    
 while n != 0:
-    res = n % 2 
-    arr.append(res)
+    #res = n % 2 
+    arr.append(n % 2)
     n = n // 2
     
 for i in range(len(arr) - 1, -1, -1):
