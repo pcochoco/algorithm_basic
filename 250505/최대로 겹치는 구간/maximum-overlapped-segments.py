f264629c0ea0a6fe9b26fcdec2f664d0891d1698
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-05-05 15:37:55 +0900

    Title: 최대로 겹치는 구간, Time: 74ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250505/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+arr = [0] * 110
+
+for i in range(n):
+    x1, x2 = map(int, input().split())
+    x1 += 100 #음수의 양수 처리
+    #-100~100까지 가능
+    x2 += 100 
+    for j in range(x1, x2):
+        #구간 - 끝점에서 닿는 경우 겹침 x 
+        arr[j] += 1 
+
+answer = 0 
+
+for i in range(110):
+    if answer < arr[i]: 
+        answer = arr[i] 
+    
+print(answer)
+
+
+
