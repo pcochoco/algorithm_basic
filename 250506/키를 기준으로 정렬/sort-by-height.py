9bf91cc9f81c5838b298ab62d282c8fb8f38a2bf
pcochoco <121675930+pcochoco@users.noreply.github.com>
2025-05-06 18:48:32 +0900

    Title: 키를 기준으로 정렬, Time: 80ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250506/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,13 @@
+
+class User:
+    def __init__(self, data):
+        self.name = data[0]
+        self.height = int(data[1]) 
+        self.weight = int(data[2])
+
+n = int(input())
+arr = [User(input().split()) for i in range(5)]
+arr.sort(key = lambda x : x.height)
+
+for i in arr:
+    print(i.name, i.height, i.weight)
